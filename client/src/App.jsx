308e331804d63e6be8ad5d5e64b33482d5a0e5bf
Svetlana <lirica1109@gmail.com>
2022-05-09 23:30:27 +0300

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { React } from "react";
 import { Routes, Route } from "react-router-dom";
 import PageLogin from './pages/PageLogin';
 import PageRegistration from "./pages/PageRegistration";
+import Navbar from "./components/Navbar/Navbar";
 // import Login from "./components/Login/Login";
 // import Registration from "./components/Registration/Registration";
 import { useEffect } from 'react';
@@ -19,12 +20,15 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
+    <div>
+      <header>
+        <Navbar/>
+      </header>
      <Routes>
         <Route path='/login' element={<PageLogin/>} />
         <Route path='/registration' element={<PageRegistration/>} />
       </Routes>
-    </>
+    </div>
   );
 }
 
