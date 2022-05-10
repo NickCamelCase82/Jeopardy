1a95e35f14a8af47d0fee497d4875eb028301fa8
Liza <lizakorneva1997@gmail.com>
2022-05-10 15:45:32 +0300

    Merged

--- client/src/components/game/Game/Game.jsx
+++ client/src/components/game/Game/Game.jsx
@@ -1,64 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Game.css';
 import Category from '../Сategory/Сategory';
 import Score from '../Score/Score';
-import Modal from '../../Modal/Modal';
+// import Modal from '../../Modal/Modal';
 
 const Game = () => {
-  const [modal, setModal] = useState(false);
+  // const [modal, setModal] = useState(false);
+  const [title1, setTitle1] = useState('');
+  const [title2, setTitle2] = useState('');
+  const [title3, setTitle3] = useState('');
+  const [title4, setTitle4] = useState('');
+  const [title5, setTitle5] = useState('');
+  const [question1, setQuestion1] = useState([]);
 
-  const gameNameOne = 'title-one';
-  const gameNameTwo = 'title-two';
-  const gameNameThree = 'title-three';
-  const gameNameFour = 'title-four';
-  const gameNameFive = 'title-five';
+  useEffect(() => {
+    (async () => {
+      const game1 = await fetch('http://localhost:3001/game1');
+      const resultGame1 = await game1.json();
+      setTitle1(resultGame1[0].category);
+      setQuestion1(resultGame1);
+      console.log(resultGame1);
+
+      const game2 = await fetch('http://localhost:3001/game2');
+      const resultGame2 = await game2.json();
+      setTitle2(resultGame2[0].category);
+
+      const game3 = await fetch('http://localhost:3001/game3');
+      const resultGame3 = await game3.json();
+      setTitle3(resultGame3[0].category);
+
+      const game4 = await fetch('http://localhost:3001/game4');
+      const resultGame4 = await game4.json();
+      setTitle4(resultGame4[0].category);
+
+      const game5 = await fetch('http://localhost:3001/game5');
+      const resultGame5 = await game5.json();
+      setTitle5(resultGame5[0].category);
+    })();
+  }, []);
+
+  // const game1 = await fetch('http://localhost:3001/game1');
+  // const resultGame1 = await game1.json();
+  // const title1 = resultGame1[0].category;
+  // console.log('title1-------------', title1);
+  // console.log('game---------------', resultGame1);
+
+  // const game2 = fetch('http://localhost:3001/game2')
+  //   .then((data) => data.json())
+  //   .then((data) => console.log(data));
+
   return (
     <div className="game">
       <div className="game-item">
-        <Category title={gameNameOne} />
+        <Category title={title1} />
         {['200', '400', '600', '800'].map((score, index) => (
-          <div onClick={() => setModal(true)}>
-            <Score score={score} key={index + 1} />
-          </div>
+          <Score score={score} question={question1} key={index + 1} />
         ))}
       </div>
       <div className="game-item">
-        <Category title={gameNameTwo} />
+        <Category title={title2} />
         {['200', '400', '600', '800'].map((score, index) => (
-          <div onClick={() => setModal(true)}>
-            <Score score={score} key={index + 1} />
-          </div>
+          <Score score={score} key={index + 1} />
         ))}
       </div>
       <div className="game-item">
-        <Category title={gameNameThree} />
+        <Category title={title3} />
         {['200', '400', '600', '800'].map((score, index) => (
-          <div onClick={() => setModal(true)}>
-            <Score score={score} key={index + 1} />
-          </div>
+          <Score score={score} key={index + 1} />
         ))}
       </div>
       <div className="game-item">
-        <Category title={gameNameFour} />
+        <Category title={title4} />
         {['200', '400', '600', '800'].map((score, index) => (
-          <div onClick={() => setModal(true)}>
-            <Score score={score} key={index + 1} />
-          </div>
+          <Score score={score} key={index + 1} />
         ))}
       </div>
       <div className="game-item">
-        <Category title={gameNameFive} />
+        <Category title={title5} />
         {['200', '400', '600', '800'].map((score, index) => (
-          <div onClick={() => setModal(true)}>
-            <Score score={score} key={index + 1} />
-          </div>
+          <Score score={score} key={index + 1} />
         ))}
       </div>
-      <Modal
-        question="Тут будет вопрос?"
-        isOpened={modal}
-        onModalClose={() => setModal(false)}
-      />
     </div>
   );
 };
--- client/src/components/game/Score/Score.jsx
+++ client/src/components/game/Score/Score.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Score.css';
+import Modal from '../../Modal/Modal';
 
-const Score = ({ score }) => {
-  return <div className="score">{score}</div>;
+const Score = ({ score /* question */ }) => {
+  const [modal, setModal] = useState(false);
+  // console.log(question);
+
+  // let quest;
+
+  // switch (score) {
+  //   case '200':
+  //     quest = question[0].question;
+  //     break;
+
+  //   case '400':
+  //     quest = question[1].question;
+  //     break;
+
+  //   case '600':
+  //     quest = question[2].question;
+  //     break;
+
+  //   case '800':
+  //     quest = question[3].question;
+  //     break;
+
+  //   default:
+  //     break;
+  // }
+
+  return (
+    <>
+      <div onClick={() => setModal(true)} className="score">
+        {score}
+      </div>
+      <Modal
+        // question={quest}
+        isOpened={modal}
+        onModalClose={() => setModal(false)}
+      />
+    </>
+  );
 };
 
 Score.propTypes = {
