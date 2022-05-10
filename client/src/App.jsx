d4da77c8716b822b122b291e92113e1a5cadcbb6
Nick <nick.egorenkov@gmail.com>
2022-05-10 05:27:15 -0700

    Main page done with logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,30 @@
-import { React } from "react";
-import { Routes, Route } from "react-router-dom";
+import { React } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import PageLogin from './pages/PageLogin';
-import PageRegistration from "./pages/PageRegistration";
+import PageRegistration from './pages/PageRegistration';
+import Main from './components/Main/Main';
 // import Login from "./components/Login/Login";
 // import Registration from "./components/Registration/Registration";
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 function App() {
-  
   const dispatch = useDispatch();
 
   useEffect(() => {
     fetch('http://localhost:3001/session', {
-      credentials: 'include'
-    }).then(data => data.json())
-      .then(user => dispatch({ type: 'USER_LOGIN', payload: user}))
+      credentials: 'include',
+    })
+      .then((data) => data.json())
+      .then((user) => dispatch({ type: 'USER_LOGIN', payload: user }));
   }, [dispatch]);
 
   return (
     <>
-     <Routes>
-        <Route path='/login' element={<PageLogin/>} />
-        <Route path='/registration' element={<PageRegistration/>} />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<PageLogin />} />
+        <Route path="/registration" element={<PageRegistration />} />
       </Routes>
     </>
   );
